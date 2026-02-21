1a2542cc1c3c46dab0bea4e719c3a1c074dc7ad6
HozefaVankanerwala <253882321+HozefaVankanerwala@users.noreply.github.com>
2026-02-21 21:33:42 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: Replace 'burhani-electrical' below with your actual GitHub repository name
-  base: '/burhani-electricals/',
-})
